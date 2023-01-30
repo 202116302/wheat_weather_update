9fdbd4f7db07070f1e0a6d5c3f8ca69158ca8af5
hyejin <wls5258@jbnu.ac.kr>
2023-01-30 16:24:35 +0900

    기상 정보 json 파일에 저장

--- weather_update.py
+++ weather_update.py
@@ -6,10 +6,16 @@ import datetime
 from json import dumps
 from flask import Flask, request, escape, render_template
 from flask_cors import cross_origin
+from tinydb import TinyDB, Query, where
 
 # 플라스크 선언
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
+
+db = TinyDB('db.json')
+Station = Query()
+
 
 try:
     from urllib import urlencode, unquote
@@ -30,13 +36,7 @@ pageNo = "1"
 numOfRaws = "1000"
 # 자료 형식 json or xml
 datatype = "JSON"
-# ----발표 날짜( 요청 날짜 , 페이지 열 때? 날짜)
-today = datetime.datetime.today().strftime("%Y%m%d")  # 오늘날짜
-y = datetime.date.today() - datetime.timedelta(days=1)
-yesterday = y.strftime("%Y%m%d")  # 어제날짜
-# ----발표 시각( 요청 시각 , 페이지 열 때? 시간)
-now = datetime.datetime.now()  # 현재 날짜, 시각
-hour = now.hour  # 현재시각
+
 # 예보지점의 x, y 좌표값
 nx_namwon = '68'
 ny_namwon = '80'
@@ -45,24 +45,10 @@ ny_namwon = '80'
 nx_iksan = '59'
 ny_iksan = '94'
 
-# ----요청 시각, 날짜 재조정
-for i in range(8):
-    if i * 3 + 2 <= hour < (i + 1) * 3 + 2:
-        hour = i * 3 + 2
-
-if hour < 2:
-    hour = 23
-    today = yesterday
-
-time_hour = f"{hour:02d}" + "00"
 
 
 # 지역별 파라미터
-new_param_namwon = {'ServiceKey': serviceKey, 'pageNo': pageNo, 'numOfRows': numOfRaws,
-             'dataType': datatype, 'base_date': today, 'base_time': time_hour, 'nx': nx_namwon, 'ny': ny_namwon}
 
-new_param_iksan = {'ServiceKey': serviceKey, 'pageNo': pageNo, 'numOfRows': numOfRaws,
-             'dataType': datatype, 'base_date': today, 'base_time': time_hour, 'nx': nx_iksan, 'ny': ny_iksan}
 
 
 
@@ -110,10 +96,12 @@ def sky(loc):
     pop_4 = []
     pop = [pop_0, pop_1, pop_2, pop_3, pop_4]
 
+
     # 날짜 뽑기
     for x in items:
         if not x['fcstDate'] in weather_date:
             weather_date.append(x["fcstDate"])
+
 
     # 강수확률(%)
     for i in range(len(weather_date)):
@@ -170,6 +158,7 @@ def sky(loc):
                 weather_total[f'{weather_date[i]}_tmin'] = f"{x['fcstValue']}°C"
                 tmin[f'{weather_date[i]}'] = f"{x['fcstValue']}°C"
 
+
     # 일 최고 기온
     for i in range(len(weather_date)):
         for x in items:
@@ -185,7 +174,7 @@ def sky(loc):
     # with open('./data.json', 'w', encoding='utf-8-sig') as f:
     #     json.dump(weather, f, ensure_ascii=False, indent=4)
 
-    return weather
+    return json.dumps(weather, ensure_ascii=False)
 
 
 # 플라스크
@@ -198,19 +187,62 @@ def home():
 @app.route("/weather",  methods=['GET'])
 @cross_origin(origin='*')
 def weather(city=None):
+    # ----발표 날짜( 요청 날짜 , 페이지 열 때? 날짜)
+    today = datetime.datetime.today().strftime("%Y%m%d")  # 오늘날짜
+    y = datetime.date.today() - datetime.timedelta(days=1)
+    yesterday = y.strftime("%Y%m%d")  # 어제날짜
+    # ----발표 시각( 요청 시각 , 페이지 열 때? 시간)
+    now = datetime.datetime.now()  # 현재 날짜, 시각
+    hour = now.hour  # 현재시각
+
+    # ----요청 시각, 날짜 재조정
+    for i in range(8):
+        if i * 3 + 2 <= hour < (i + 1) * 3 + 2:
+            hour = i * 3 + 2
+
+    if hour < 2:
+        hour = 23
+        today = yesterday
+
+    time_hour = f"{hour:02d}" + "00"
+
+    new_param_namwon = {'ServiceKey': serviceKey, 'pageNo': pageNo, 'numOfRows': numOfRaws,
+                        'dataType': datatype, 'base_date': today, 'base_time': time_hour, 'nx': nx_namwon,
+                        'ny': ny_namwon}
+
+    new_param_iksan = {'ServiceKey': serviceKey, 'pageNo': pageNo, 'numOfRows': numOfRaws,
+                       'dataType': datatype, 'base_date': today, 'base_time': time_hour, 'nx': nx_iksan, 'ny': ny_iksan}
+
     if request.method == 'GET':
         city = request.args.get('city')
     city = str(escape(city))
+
     if city == '남원':
-        return sky(new_param_namwon)
+        today_weather = db.search((where('name') == "namwon") & (where('date') == today))
     elif city == "익산":
-        return sky(new_param_iksan)
+        today_weather = db.search((where('name') == "iksan") & (where('date') == today))
     else:
-        return "해당지역없음"
+        today_weather = []
+
+    if len(today_weather) > 0:  # 오늘날짜 / 남원 혹은 익산 자료가 있으면, 있는 자료로 리턴
+        return today_weather[0]['json_content']
+    else:
+        if city == '남원':
+            json_content = sky(new_param_namwon)
+            db.insert({"name": "namwon", "date": today, "json_content": json_content})
+            return json_content
+        elif city == "익산":
+            json_content = sky(new_param_iksan)
+            db.insert({"name": "iksan", "date": today, "json_content": json_content})
+            return json_content
+        else:
+            return "해당지역없음"
 
 
 def main():
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+
+
 
 
 if __name__ == '__main__':
