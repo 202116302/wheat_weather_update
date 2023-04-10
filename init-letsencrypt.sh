94a8d721e6a99d31b6403919d5d2d83bbaaa3330
202116302 <wls5258@naver.com>
2023-04-10 17:49:36 +0900

    이메일 변경

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(jbnu-smartfarm.kr)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="wls5258@jbnu.ac.kr" # Adding a valid address is strongly recommended
+email="wls5258@naver.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
