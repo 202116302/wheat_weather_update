771df227bff2a933b654c372901d2d08fb571e4a
202116302 <wls5258@naver.com>
2023-07-31 13:32:20 +0900

    날짜수정

--- weather_update.py
+++ weather_update.py
@@ -378,9 +378,9 @@ def weather_mid(city=None):
     nam_weather_mid = json.dumps(weather_mid, ensure_ascii=False)
 
     if city == 'namwon':
-        future_weather = db3.search((where('name') == "namwon") & (where('date') == today))
+        future_weather = db3.search((where('name') == "namwon") & (where('date') == now))
     elif city == 'iksan':
-        future_weather = db3.search((where('name') == "iksan") & (where('date') == today))
+        future_weather = db3.search((where('name') == "iksan") & (where('date') == now))
     else:
         future_weather = []
 
@@ -388,7 +388,7 @@ def weather_mid(city=None):
         return future_weather[0]['json_content']
     else:
         if city == 'namwon':
-            db3.insert({"name": "namwon", "date": today, 'json_content': nam_weather_mid})
+            db3.insert({"name": "namwon", "date": now, 'json_content': nam_weather_mid})
             return nam_weather_mid
         elif city == "iksan":
             db3.insert({"name": "iksan", "date": today})
