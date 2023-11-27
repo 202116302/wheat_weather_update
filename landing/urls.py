4389b2542bdd25100874ac2ba48b661cd6e18db0
202116302 <wls5258@naver.com>
2023-11-27 22:43:26 +0900

    익산 대시보드 이미지 추가 , 수정

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing)
-
+    path('', views.landing),
+    path('2', views.landing2),
+    path('buan', views.landing3)
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,5 +5,12 @@ def landing(request):
     return render(request, "landing/iksan.html")
 
 
+def landing2(request):
+    return render(request, "landing/iksan2.html")
+
+
+def landing3(request):
+    return render(request, "landing/buan.html")
+
 def sample(request):
     return render(request, "landing/sample.html")
