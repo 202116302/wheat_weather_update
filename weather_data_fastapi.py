20cd74cf9b62e6fa6acfc8b38131743a33c7b678
202116302 <wls5258@naver.com>
2024-04-16 21:09:39 +0900

    포트번호 변경

--- weather_data_fastapi.py
+++ weather_data_fastapi.py
@@ -491,8 +491,8 @@ async def home(request: Request):
 
 
 def main():
-    uvicorn.run(app, host="127.0.0.1", port=5000)
-    # uvicorn.run(app, host="0.0.0.0", port=7500)
+    # uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=7500)
 
 
 if __name__ == '__main__':
